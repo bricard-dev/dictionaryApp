4aa58a71cc8e5e1d4a5605fe0313b815d4edbbef
Bastien Ricard <bastienricard@macbook-pro-de-bastien.home>
2021-10-28 17:06:41 +0200

    Add audio feature

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
-const audio = document.getElementById("sound");
+const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () => {
@@ -13,7 +13,7 @@ btn.addEventListener("click", () => {
         result.innerHTML = `
       <div class="word">
         <h3>${data[0].word}</h3>
-        <button><i class="fas fa-volume-up"></i></button>
+        <button onclick="playSound()"><i class="fas fa-volume-up"></i></button>
       </div>
       <div class="details">
         <p>${data[0].meanings[0].partOfSpeech}</p>
@@ -25,6 +25,14 @@ btn.addEventListener("click", () => {
       <p class="word-example">
         ${data[0].meanings[0].definitions[0].example || ""}
       </p>`;
+        sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+      })
+      .catch(() => {
+        result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
       });
   }
 });
+
+var playSound = () => {
+  sound.play();
+};
