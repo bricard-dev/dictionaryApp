891e6b2714c47a6082471f3d758c960d83514874
Bastien Ricard <bastienricard@macbook-pro-de-bastien.home>
2021-10-30 16:19:33 +0200

    Convert javascript vanilla to jQuery

--- js/script.js
+++ js/script.js
@@ -1,41 +1,50 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const result = document.getElementById("result");
-const sound = document.getElementById("sound");
-const btn = document.getElementById("search-btn");
+const result = $("#result");
+const sound = $("#sound");
+const btn = $("#search-btn");
 
-document.addEventListener("keyup", (e) => {
+$(document).keyup((e) => {
   if (e.keyCode === 13) {
     btn.click();
   }
 });
 
-btn.addEventListener("click", () => {
-  let inpWord = document.getElementById("inp-word").value;
-  fetch(`${url}${inpWord}`)
-    .then((response) => response.json())
-    .then((data) => {
-      result.innerHTML = `
-      <div class="word">
-        <h3>${data[0].word}</h3>
-        <button onclick="playSound()"><i class="fas fa-volume-up"></i></button>
-      </div>
-      <div class="details">
-        <p>${data[0].meanings[0].partOfSpeech}</p>
-        <p>/${data[0].phonetic}/</p>
-      </div>
-      <p class="word-meaning">
-        ${data[0].meanings[0].definitions[0].definition}
-      </p>
-      <p class="word-example">
-        ${data[0].meanings[0].definitions[0].example || ""}
-      </p>`;
-      sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+btn.click(() => {
+  let inpWord = $("#inp-word").val();
+  if (inpWord !== "") {
+    $.get({
+      url: url + inpWord,
+      dataType: "json",
     })
-    .catch(() => {
-      result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
-    });
+      .done((data) => {
+        result.html(
+          `
+          <div class="word">
+            <h3>${data[0].word}</h3>
+            <button onclick="playSound()"><i class="fas fa-volume-up"></i></button>
+          </div>
+          <div class="details">
+            <p>${data[0].meanings[0].partOfSpeech}</p>
+            <p>/${data[0].phonetic}/</p>
+          </div>
+          <p class="word-meaning">
+            ${data[0].meanings[0].definitions[0].definition}
+          </p>
+          <p class="word-example">
+            ${data[0].meanings[0].definitions[0].example || ""}
+          </p>
+          `
+        );
+        sound.attr("src", `https:${data[0].phonetics[0].audio}`);
+      })
+      .fail(() => {
+        result.html(`<h3 class="error">Couldn't Find The Word</h3>`);
+      });
+  } else {
+    result.html(`<h3 class="error">Couldn't Find The Word</h3>`);
+  }
 });
 
 var playSound = () => {
-  sound.play();
+  sound.trigger("play");
 };
